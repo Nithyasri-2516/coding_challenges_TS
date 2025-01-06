2e2ad51b15c3ea953edaf5c1cedcf5aef5d55bd8
Nithyasri-2516 <nithyasri1625@gmail.com>
2025-01-06 14:53:15 +0530

    add subArray-ts

---
+++ day26_jan6/subArray.js
@@ -0,0 +1,30 @@
+// 560. Subarray Sum Equals K
+// Medium
+// Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
+// A subarray is a contiguous non-empty sequence of elements within an array.
+// Example 1:
+// Input: nums = [1,1,1], k = 2
+// Output: 2
+// Example 2:
+// Input: nums = [1,2,3], k = 3
+// Output: 2
+// Example 3:
+// Input
+// nums = [1,2,3,6,4,2,0,4]
+// k = 6
+// Output:
+// 5
+function subarrays(input, key) {
+    var count = 0;
+    for (var i = 0; i < input.length; i++) {
+        var sum = 0;
+        for (var j = i; j < input.length; j++) {
+            sum = sum + input[j];
+            if (sum == key) {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+console.log(subarrays([1, 2, 3, 6, 4, 2, 0, 4], 6));
